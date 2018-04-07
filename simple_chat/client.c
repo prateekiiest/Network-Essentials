c7335ca94cbc12d41e65c3de3de207de53ec4b91
Prateek Chanda <prateekkol21@gmail.com>
2018-04-07 21:04:52 +0530

    Simple Chat Update

--- simple_chat/client.c
+++ simple_chat/client.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
-#include<netinet/in.h>
 #include<sys/types.h>
-#include<sys/socket.h>
+#include<sys/socket.h> // structures needed for socket
 #include<netdb.h>
 #include<string.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <netinet/in.h> // structures needed for internet domain addresses
 #include <arpa/inet.h>
 
 #include<stdlib.h>
-#define MAX 180
-#define PORT 8080
+#define MAX 1024 // the maximum size of the data
+#define PORT 8080  // defined port no.
 #define SA struct sockaddr
 
+// function for operations on the message data
 void func(int sockfd)
 {
 	char data[MAX];
@@ -26,7 +26,7 @@ void func(int sockfd)
 	while((data[n++]=getchar())!='\n');  //  while not end of line
 	write(sockfd,data,sizeof(data));  // write the data from the terminal
 	bzero(data,sizeof(data));  // again reinitialize data back to 0
-	read(sockfd,data,sizeof(data));
+	read(sockfd,data,sizeof(data)); // read the data back into the buffer
 	printf("From Prateek : %s",data);
 
  	if((strncmp(data,"quit",4))==0)  // if computer types in `quit`, exit the conversation
@@ -41,33 +41,33 @@ void func(int sockfd)
 
 int main()
 {
+	//defining the socket file descriptors
 	int sockfd,connfd;
+	//a structure containing an internet address.
+	//This structure is defined in <netinet/in.h>
 	struct sockaddr_in servaddr,cli;
-	sockfd=socket(AF_INET,SOCK_STREAM,0);
+	sockfd=socket(AF_INET,SOCK_STREAM,0); // creating the socket
 
-        // Creating socket
-	if(sockfd==-1)
-	{
+	// if socket creation failed file descriptor returned will be -1
+	if(sockfd==-1)	{
 		printf("socket creation failed...\n");
-		exit(0);
-	}
-
+		exit(0); } // exit abnormally from the program
 	else
-  		printf("Socket successfully created..\n");
+  		printf("Socket successfully created..\n"); // else socket created successfully
 
-
+	// initialize the serve_addr to 0
 	bzero(&servaddr,sizeof(servaddr));
+	// Setting the functionalities of the structure
+	// setting the address family and the port no.
 	servaddr.sin_family=AF_INET;
 	servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
 	servaddr.sin_port=htons(PORT);
 
-
-
+    // connect to the socket with the given sock_fd and server address
 	if(connect(sockfd,(SA *)&servaddr,sizeof(servaddr))!=0)
-	{
- 		 // connection failed with server
+	{ // if error then return value non-zerp
 		printf("connection with the server failed...\n");
-		exit(0);
+		exit(0); // exit the program abnormally
 	}
 
 	else
--- simple_chat/server.c
+++ simple_chat/server.c
@@ -1,48 +1,40 @@
 #include<stdio.h>
-#include<netinet/in.h>
+#include<netinet/in.h> // structures needed for internet domain addresses
 #include <unistd.h>
 #include<sys/types.h>
-#include<sys/socket.h>
+#include<sys/socket.h> // structures needed for internet domain addresses
 #include<netdb.h>
 #include<stdlib.h>
 #include<string.h>
 
-#define MAX 180
-#define PORT 8080
+#define MAX 1024 // the maximum size of the data
+#define PORT 8080 // defined port no.
 #define SA struct sockaddr
 
-void msg(int sockfd)
-{
-	char data[MAX];
-
+// function for operations on the message data
+void msg(int sockfd){
+	char data[MAX];  // data array
 	int n;
-// int flag = 0 ;
 
 	for(;;)  // infinite loop
 	{
  		bzero(data,MAX);  // initialize all the data to 0
  		read(sockfd,data,sizeof(data));  //  read the data
-
- 	// CLient =  Computer
+	 	// CLient =  Computer
         //  Server = Prateek
-
         // CLient prints the message data
 		printf("Computer says: %s\t", data);
  		bzero(data,MAX);
 		n=0;
-
-
 		// Until the user enters into a new line
 		while((data[n++]=getchar())!= '\n');
 		// Write from the server side
 			write(sockfd,data,sizeof(data));
-
         // if data = 'quit'
-
     	if((strncmp("quit",data,4))==0)//  if the server/ client says quit, exit
 		{
                     // if data == quit exit the conversation
-		printf("Prateek Exits conversation...\n");
+			printf("Prateek Exits conversation...\n");
 			break;
 		}
  	}
@@ -56,36 +48,31 @@ void msg(int sockfd)
 int main()
 
 {
-
+    // the socket file descriptors defined
 	int sockfd,connfd,len;
-
+ 	//a structure containing an internet address.
+	//This structure is defined in <netinet/in.h>
 	struct sockaddr_in servaddr,cli;
 
-
-
-        int new_socket;
-
-        sockfd = socket(AF_INET, SOCK_STREAM,0);  // get the socket file descriptor
-	if(sockfd==-1)
-	{
-		printf("socket creation failed...\n");
-		exit(0);
+	sockfd = socket(AF_INET, SOCK_STREAM,0); // creating the socket
+	if(sockfd==-1) // if socket creation failed file descriptor returned will be -1
+	{	printf("socket creation failed...\n");
+		exit(0); // exit the program abnormally
 	}
 	else
 		printf("Socket successfully created..\n");
 
+	// initialize the serve_addr to 0
 	bzero(&servaddr,sizeof(servaddr));
+	// Setting the functionalities of the structure
+	// setting the address family and the port no.
  	servaddr.sin_family=AF_INET;
 	servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
 	servaddr.sin_port=htons(PORT);
 
-
-
-
-	if((listen(sockfd,5))!=0)
-	{
+	if((listen(sockfd,5))!=0){ // listen to the socketfd for incoming c
 		printf("Listen failed...\n");
-		exit(0);
+		exit(0);// exit the program abnormally
 	}
 
 	else
@@ -93,22 +80,17 @@ int main()
 
 
 	len=sizeof(cli);
+	// extracts  the  first   connection
+    //  request  on  the queue of pending connections for the listening socket
 	connfd=accept(sockfd,(SA *)&cli,&len);
-	if(connfd<0)
-	{
+	if(connfd<0)	{
 		printf("Acceptance failed ...\n");
 		exit(0);
 	}
-
 	else
 		printf("Acccepting the client...\n");
 
 	msg(connfd);
-
-    	//pthread_create( &tid, NULL, msg, new_socket);
-
-
-
 	 int flag = 1;
    	 while(flag) {
      		 char dat[256];
@@ -117,15 +99,8 @@ int main()
        		 send(new_socket , dat , strlen(dat) , 0 );
 	if (dat[0]=='a') {
            flag=0;
-
        }
     }
-
-
-
-
-
-
 	close(sockfd);
 
 	}
